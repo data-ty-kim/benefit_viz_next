4e6fe362ae4f2aa519b9823fbe2632c5d4abfc74
Maggie Siow <siowmaggie97@gmail.com>
2024-11-07 15:45:22 +0900

    updating fig_area_2_code

--- src/fig_area_2.py
+++ src/fig_area_2.py
@@ -48,24 +48,35 @@ def fig_area_2(df):
     fig_area_2.update_xaxes(range=(-1,7.5),visible= False)
     fig_area_2.update_yaxes(range=(-3,11),visible= False,)
     fig_area_2.update_legends(visible=False)
-    fig_area_2.update_layout(
-                legend=dict(
-                    orientation="h",
-                    y=-0.3,
-                    x=-0.15,
-                    title_font_family="NanumSquare",
-                    font=dict(
-                        family="NanumSquare",
-                        size=14,
-                        color="#919191"
-                    )
-                ),
-                margin=dict(t=0,b=0,r=0,l=0),
-                plot_bgcolor='#fff4f4',
-                paper_bgcolor='#fff4f4',
-                xaxis_fixedrange=True, 
-                yaxis_fixedrange=True,
-                width=325,
-                height=450
-            )
+    if df.empty:
+        fig_area_2.update_layout(
+            font={'family': 'NanumSquare'},
+            annotations=[dict(
+                text="<span style='font-size: 1.4rem'>장학금 및 연구비<br>수혜 내역이 없습니다.</span>",
+                x=2.5, y=1.5,
+                xanchor="center", yanchor="middle",
+                showarrow=False,
+                font=dict(color="#252930", size=32, family="NanumSquare")
+            )])
+    else:
+        fig_area_2.update_layout(
+                    legend=dict(
+                        orientation="h",
+                        y=-0.3,
+                        x=-0.15,
+                        title_font_family="NanumSquare",
+                        font=dict(
+                            family="NanumSquare",
+                            size=14,
+                            color="#919191"
+                        )
+                    ),
+                    margin=dict(t=0,b=0,r=0,l=0),
+                    plot_bgcolor='#F9F9F9',
+                    paper_bgcolor='#F9F9F9',
+                    xaxis_fixedrange=True, 
+                    yaxis_fixedrange=True,
+                    width=325,
+                    height=450
+                )
     return fig_area_2
